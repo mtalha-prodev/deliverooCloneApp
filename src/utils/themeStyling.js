4bfb156cd396e7393d4e53cd91f5b6eebd6ed990
Muhammad Talha <mtalha0018@gmail.com>
2023-07-13 02:35:19 +0500

    add utils

---
+++ src/utils/themeStyling.js
@@ -0,0 +1,18 @@
+export const backgroundStyle = 'bg-neutral-200 px-4 dark:bg-slate-900';
+
+export const theme = {
+  bgColor: '',
+  titleColor: '',
+  normalTextColor: '',
+  btnTextColor: '',
+  btnBgColor: '',
+  iconColor: '#00CCBB',
+};
+
+export const fontFamily = {
+  textSize: '',
+  titleSize: '',
+  btnTextSize: '',
+  screenMargin: '',
+  screenPadding: '',
+};
